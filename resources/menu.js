1f8c1047ce32e7fd3449cc70c3876f1b6032ffe3
Andrew Garrett <agarrett@wikimedia.org>
2015-01-14 14:02:53 +0100

    Hide hamburger menu on click outside it

--- resources/menu.js
+++ resources/menu.js
@@ -3,4 +3,10 @@ $( function () {
 		e.preventDefault();
 		$( '#site-wrap' ).toggleClass( 'site-shift' );
 	} );
+
+	$( '#site-wrap' ).click( function( e ) {
+		if ( ! $( e.target ).is( '.navbar-header, .navbar-header *' ) ) {
+			$( '#site-wrap' ).removeClass( 'site-shift' );
+		}
+	} );
 } );
